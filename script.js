4cef2f189201411ebf95f6a28fa7f3523ba00058
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-25 13:05:29 -0300

    final project

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
+let userCounter = 0;
+let pcCounter = 0; 
+let userName = "User";
+// let userName = prompt("Give your name or:","User");
 
-   let userCounter = 0;
-   let pcCounter = 0;
-//    let userName = "prompt("What is your name? ", "User");"
-  
-let userName = "User"
+// listeners for user's choise
 let buttonR = document.getElementById('btnR');
 let buttonP = document.getElementById('btnP');
 let buttonS = document.getElementById('btnS');
+// listeners for pc's choise
+let pcR = document.getElementById('divR');
+let pcP = document.getElementById('divP');
+let pcS = document.getElementById('divS');
+//listeners for hidding images and borders after match is over
+let divButtons = document.getElementById('buttons');
+let divPCHands = document.getElementById('options');
+
+
+
 let userInput = "";
 
-
-function game(){
- 
-    
-
+function game(){ 
+document.getElementById('happenningP').innerText = "Choose an option";
+startGame.remove();
 function getComputerChoice(){
     let rockPaperScissor = ["ROCK", "PAPER" , "SCISSORS"];
     //here the computer is randomly choosing its option using Math.random
     let computerSelection = rockPaperScissor[Math.floor(Math.random()*rockPaperScissor.length)];
     return computerSelection;
-}
+} 
+
+// getComputerChoice
 let computerSelection= "";
 
 // listening to button's clicks:
@@ -28,19 +38,46 @@ btnP.addEventListener('click', roundRPS);
 btnS.addEventListener('click', roundRPS);
 
 
+   // creates a class for computer's hands transition in CSS
+
+function computerTransitions(e){
+ 
+    if(computerSelection === "ROCK"){
+divR.className= 'pcTransition';
+divP.className= 'pcHandHidden';
+divS.className= 'pcHandHidden';
+
+        
+    }else if(computerSelection === "PAPER"){
+        divR.className= 'pcHandHidden';
+        divP.className= 'pcTransition';
+        divS.className= 'pcHandHidden';
+        
+       
+    }else if(computerSelection === "SCISSORS"){
+        divR.className= 'pcHandHidden';
+        divP.className= 'pcHandHidden';
+        divS.className= 'pcTransition';
+        
+    }
+}
+
+
+
 function roundRPS(e){
     if(e.target['id'] === "btnR"){
     userInput = "ROCK";
     console.log(userInput);
     oneRound()
     console.log(`Computer:${computerSelection}`);
+    computerTransitions()
     }
     else if(e.target['id'] === "btnP"){
     userInput = "PAPER";
     console.log(userInput);
     oneRound()
     console.log(`Computer:${computerSelection}`);
-
+    computerTransitions()
 
     }
     else if(e.target['id'] === "btnS"){
@@ -48,10 +85,10 @@ function roundRPS(e){
     console.log(userInput);
     oneRound()
     console.log(`Computer:${computerSelection}`);
-
+    computerTransitions()
 
     }
-}
+} // roundRPS
 
 function oneRound(){
 
@@ -59,19 +96,27 @@ function oneRound(){
 computerSelection= getComputerChoice();
     
     let playerSelection = userInput;
-    
+    // checks the selections and prints to the screen 
     if (playerSelection === computerSelection){
+        let tie = `It's a tie! ${playerSelection} is igual to ${computerSelection}`
+document.getElementById('happenningP').innerText = tie;
     } else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS'){
+        document.getElementById('happenningP').innerText =  "You win! ROCK beats SCISSORS!";
     userCounter++;
     } else if (playerSelection == 'ROCK' && computerSelection == 'PAPER'){
+        document.getElementById('happenningP').innerText = "You lose! PAPER beats ROCK!";
     pcCounter++;
     } else if (playerSelection == 'PAPER' && computerSelection == 'ROCK'){
+        document.getElementById('happenningP').innerText = "You win! PAPER beats ROCK!";
     userCounter++;
     } else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS'){
+        document.getElementById('happenningP').innerText = "You lose! SCISSORS beats PAPER!";
     pcCounter++;
     } else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER'){
     userCounter++;
+    document.getElementById('happenningP').innerText = "You win! SCISSORS beats PAPER!";
     } else if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
+        document.getElementById('happenningP').innerText = "You lose! ROCK beats SCISSORS!";
     pcCounter++; }
     let userScore = `Your score is:${userCounter}`;
     let pcScore = `The computer score is: ${pcCounter}`;
@@ -80,10 +125,42 @@ computerSelection= getComputerChoice();
     document.getElementById('scoreUser').innerText = userScore;
     document.getElementById('scorePC').innerText = pcScore;
    
-    }
+    //remove items after match is over: 
+    function removeItemOver(){
+        divPCHands.style.display= "none";
+        divButtons.style.display= "none";
+        buttonR.remove();
+        buttonP.remove();
+        buttonS.remove();
+        happenningP.remove();
+            }
+   if(pcCounter === 5 || userCounter === 5){
+    if(pcCounter < userCounter){
+        document.getElementById('scoreUser').innerText = `${userName} won`;
+        document.getElementById('scorePC').innerText = "";
+        removeItemOver();
+        // here add an event that sets all the images and buttons to display: 'none',
+        // and show an image of a user breaking a pc
+
+       }
+       else if(pcCounter > userCounter){
+        document.getElementById('scorePC').innerText = "Computer won";
+         document.getElementById('scoreUser').innerText = "";
+         removeItemOver();
+ // here add an event that sets all the images and buttons to display: 'none';
+ // and show an image of a pc taking the world..lol
+   } 
+ }
+
+} // oneRound
+
+} // game()
+    
+
+// Reload everything:
+function reload() {
+    reload = location.reload();
 }
-    
-    
 
 
 
