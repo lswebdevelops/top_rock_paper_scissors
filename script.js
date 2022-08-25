3b4c2189e327758080dca28bc9d7ab8191dd7c2f
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-24 23:32:38 -0300

    score and conditionals working

--- script.js
+++ script.js
@@ -1,77 +1,94 @@
 
    let userCounter = 0;
    let pcCounter = 0;
-   let userName = prompt("What is your name? ", "User");
+//    let userName = "prompt("What is your name? ", "User");"
   
+let userName = "User"
+let buttonR = document.getElementById('btnR');
+let buttonP = document.getElementById('btnP');
+let buttonS = document.getElementById('btnS');
+let userInput = "";
+
 
 function game(){
-    for(let i = 0; i <=4; i++){
+ 
     
-//here starting the functions: 
+
 function getComputerChoice(){
     let rockPaperScissor = ["ROCK", "PAPER" , "SCISSORS"];
     //here the computer is randomly choosing its option using Math.random
     let computerSelection = rockPaperScissor[Math.floor(Math.random()*rockPaperScissor.length)];
     return computerSelection;
-       
 }
-let computerSelection= getComputerChoice();
+let computerSelection= "";
 
-function roundRPS(){
+// listening to button's clicks:
+btnR.addEventListener('click', roundRPS);
+btnP.addEventListener('click', roundRPS);
+btnS.addEventListener('click', roundRPS);
+
+
+function roundRPS(e){
+    if(e.target['id'] === "btnR"){
+    userInput = "ROCK";
+    console.log(userInput);
+    oneRound()
+    console.log(`Computer:${computerSelection}`);
+    }
+    else if(e.target['id'] === "btnP"){
+    userInput = "PAPER";
+    console.log(userInput);
+    oneRound()
+    console.log(`Computer:${computerSelection}`);
+
+
+    }
+    else if(e.target['id'] === "btnS"){
+    userInput = 'SCISSORS';
+    console.log(userInput);
+    oneRound()
+    console.log(`Computer:${computerSelection}`);
+
+
+    }
+}
+
+function oneRound(){
+
+    // gets the computer's selection
+computerSelection= getComputerChoice();
     
-let userInput = prompt("Type rock, paper or scissors","paper");
-let playerSelection = userInput.toUpperCase();
-if (playerSelection === computerSelection){
-let tie = `It's a tie! ${playerSelection} is igual to ${computerSelection}`
-alert(tie);
-} else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS'){
-alert( "You win! ROCK beats SCISSORS!");
-userCounter++;
-} else if (playerSelection == 'ROCK' && computerSelection == 'PAPER'){
-alert("You lose! PAPER beats ROCK!");
-pcCounter++;
-} else if (playerSelection == 'PAPER' && computerSelection == 'ROCK'){
-alert("You win! PAPER beats ROCK!");
-userCounter++;
-} else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS'){
-alert("You lose! SCISSORS beats PAPER!");
-pcCounter++;
-} else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER'){
-alert( "You win! SCISSORS beats PAPER!");
-userCounter++;
-} else if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
-alert( "You lose! ROCK beats SCISSORS!");
-pcCounter++;
+    let playerSelection = userInput;
+    
+    if (playerSelection === computerSelection){
+    } else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS'){
+    userCounter++;
+    } else if (playerSelection == 'ROCK' && computerSelection == 'PAPER'){
+    pcCounter++;
+    } else if (playerSelection == 'PAPER' && computerSelection == 'ROCK'){
+    userCounter++;
+    } else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS'){
+    pcCounter++;
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER'){
+    userCounter++;
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
+    pcCounter++; }
+    let userScore = `Your score is:${userCounter}`;
+    let pcScore = `The computer score is: ${pcCounter}`;
 
-}else{
-    alert("Please type 'rock', 'paper' or 'scissors'." );
+    //prints the result to screen:
+    document.getElementById('scoreUser').innerText = userScore;
+    document.getElementById('scorePC').innerText = pcScore;
+   
+    }
 }
-let userScore = `Your score is:${userCounter}`;
-let pcScore = `The computer score is: ${pcCounter}`;
-alert(pcScore);
-alert(userScore);
-}
-roundRPS();
-        
-// end of functions in the loop
-        
-}
-//after 5 turns it prints:
-
-
-}
-game()
+    
+    
 
 
 
-if(pcCounter < userCounter){
-    alert(`${userName} wins`);
-}
-else if(pcCounter > userCounter){
-    alert("Pc wins");
-}
-else{
-    alert("It's a tie")
-}
 
-alert("End of Game");
+
+
+
+
